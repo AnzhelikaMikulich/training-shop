984c4d39cc4598c2a705b4b05e46b5e305ae1d11
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2022-03-07 01:16:05 +0300

    feat:add discount for products

--- src/components/Main/WomenMen/MenAsortment.jsx
+++ src/components/Main/WomenMen/MenAsortment.jsx
@@ -15,7 +15,16 @@ const MenAsortment = () => {
             data-test-id={`clothes-card-men`}
           >
             <div className="womens-block-asortment-item">
-              <div>
+              <div className="womens-block-asortment-img">
+                <div
+                  className={
+                    item.discount === null
+                      ? "womens-block-asortment-discount-none"
+                      : "womens-block-asortment-discount"
+                  }
+                >
+                  {item.discount}
+                </div>
                 <img
                   src={`https://training.cleverland.by/shop${item.images[0].url}`}
                   alt="asortment-img"
@@ -24,7 +33,26 @@ const MenAsortment = () => {
               </div>
               <p className="asortment-title">{item.name}</p>
               <div className="asortment-description">
-                <p className="asortment-description-price">{`$ ${item.price}`}</p>
+                <p className="asortment-description-price">
+                  {`$ ${
+                    item.discount === null
+                      ? item.price
+                      : (
+                          item.price -
+                          (item.price / 100) *
+                            parseInt(item.discount.match(/\d+/), 10)
+                        ).toFixed(2)
+                  }`}
+                  <span
+                    className={
+                      item.discount != null
+                        ? "asortment-description-discount"
+                        : "asortment-description-discount-none"
+                    }
+                  >
+                    ${item.price}
+                  </span>
+                </p>
                 <div className="asortment-description-score">
                   <img src={star} alt="score" />
                   <img src={star} alt="score" />
--- src/components/Main/WomenMen/WomenAsortment.jsx
+++ src/components/Main/WomenMen/WomenAsortment.jsx
@@ -16,7 +16,17 @@ const WomenAsortment = () => {
               data-test-id={`clothes-card-women`}
             >
               <div className="womens-block-asortment-item">
-                <div>
+                <div className="womens-block-asortment-img">
+                  <div
+                    className={
+                      item.discount === null
+                        ? "womens-block-asortment-discount-none"
+                        : "womens-block-asortment-discount"
+                    }
+                  >
+                    
+                    {item.discount}
+                  </div>
                   <img
                     src={`https://training.cleverland.by/shop${item.images[0].url}`}
                     alt="asortment-img"
@@ -25,7 +35,26 @@ const WomenAsortment = () => {
                 </div>
                 <p className="asortment-title">{item.name}</p>
                 <div className="asortment-description">
-                  <p className="asortment-description-price">{`$ ${item.price}`}</p>
+                  <p className="asortment-description-price">
+                    {`$ ${
+                      item.discount === null
+                        ? item.price
+                        : (
+                            item.price -
+                            (item.price / 100) *
+                              parseInt(item.discount.match(/\d+/), 10)
+                          ).toFixed(2)
+                    }`}
+                    <span
+                      className={
+                        item.discount != null
+                          ? "asortment-description-discount"
+                          : "asortment-description-discount-none"
+                      }
+                    >
+                      ${item.price}
+                    </span>
+                  </p>
                   <div className="asortment-description-score">
                     <img src={star} alt="score" />
                     <img src={star} alt="score" />
