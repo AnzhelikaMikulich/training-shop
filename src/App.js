ed4db7b0de7a4d145c673f2a941016effc6c0d51
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2022-03-08 02:33:10 +0300

    feat:add unique model colors, color and size choices

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
-import React from 'react';
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import Header from './components/Header/Header';
-import Main from './pages/Main';
-import Footer from './components/Footer/Footer';
-import ProductPage from './pages/ProductPage';
-import ClothesWomen from './pages/ClothesWomen';
-import ClothesMen from './pages/ClothesMen';
-import { PRODUCTS } from './constants/data';
+import React from "react";
+import { HashRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Main from "./pages/Main";
+import Footer from "./components/Footer/Footer";
+import ProductPage from "./pages/ProductPage";
+import ClothesWomen from "./pages/ClothesWomen";
+import ClothesMen from "./pages/ClothesMen";
+import { PRODUCTS } from "./constants/data";
 
 function App() {
-  const ProductPageRoutes = Object.values(PRODUCTS).map((items)=>items.map((item)=>(
-    <Route exact path={`/${item.category}/${item.id}`}key={item.id} element={<ProductPage product={item} type={item.category}/>} />
-
-  )))
+  const ProductPageRoutes = Object.values(PRODUCTS).map((items) =>
+    items.map((item) => (
+      <Route
+        exact
+        path={`/${item.category}/${item.id}`}
+        key={item.id}
+        element={<ProductPage product={item} type={item.category}  />}
+      />
+    ))
+  );
 
   return (
     <HashRouter>
-      <div className="app" data-test-id='app'>
-      <div className="wrapper">
-      <Header></Header>
+      <div className="app" data-test-id="app">
+        <div className="wrapper">
+          <Header></Header>
 
-      <Routes>
-          <Route exact path="/" element={<Main />} />
-          <Route exact path="/women" element={<ClothesWomen />} />
-          <Route exact path="/men" element={<ClothesMen />} />
-          {ProductPageRoutes}
-        </Routes>
-        
-    <Footer></Footer>
-  </div>
-    </div>
+          <Routes>
+            <Route exact path="/" element={<Main />} />
+            <Route exact path="/women" element={<ClothesWomen />} />
+            <Route exact path="/men" element={<ClothesMen />} />
+            {ProductPageRoutes}
+          </Routes>
+
+          <Footer></Footer>
+        </div>
+      </div>
     </HashRouter>
-    
-    
   );
 }
 
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import './style/clothesstyle.css';
-import './style/productstyle.css';
-import ProductSlider from '../components/Sliders/ProductSlider/ProductSlider';
-import RelatedSlider from '../components/Sliders/RelatedSlider/RelatedSlider';
-import FormFooter from '../components/Main/Form/FormFooter';
-import PaySystem from '../components/ProductPage/PaySystem';
-import arrovNext from '.././assets/image/pages/arrov.png'
-import star from '.././assets/image/women/star.png'
-import model1 from '.././assets/image/productpage/model1.png'
-import model2 from '.././assets/image/productpage/model2.png'
-import model3 from '.././assets/image/productpage/model3.png'
-import model4 from '.././assets/image/productpage/model4.png'
-import shipping from '.././assets/image/banner/shipping.png'
-import return1 from '.././assets/image/banner/return.png'
-import send from '.././assets/image/productpage/send.png'
-import heart from '.././assets/image/productpage/heart.png'
-import scales from '.././assets/image/productpage/scales.png'
+import React from "react";
+import { useState } from "react";
+import "./style/clothesstyle.css";
+import "./style/productstyle.css";
+import ProductSlider from "../components/Sliders/ProductSlider/ProductSlider";
+import RelatedSlider from "../components/Sliders/RelatedSlider/RelatedSlider";
+import FormFooter from "../components/Main/Form/FormFooter";
+import PaySystem from "../components/ProductPage/PaySystem";
+import arrovNext from ".././assets/image/pages/arrov.png";
+import star from ".././assets/image/women/star.png";
+import shipping from ".././assets/image/banner/shipping.png";
+import return1 from ".././assets/image/banner/return.png";
+import send from ".././assets/image/productpage/send.png";
+import heart from ".././assets/image/productpage/heart.png";
+import scales from ".././assets/image/productpage/scales.png";
 
+const ProductPage = ({ type, product }) => {
+  const [activeIdColor, setActiveIdColor] = useState(product.images[0].id);
+  const [activeColor, setActiveColor] = useState(product.images[0].color);
+  const [activeSize, setActiveSize] = useState(product.sizes[0]);
+  let uniqueColors = new Set(product.images.map(({ color }) => color));
 
-const ProductPage = (props) => {
   return (
-    <div className='page-product' data-test-id={`product-page-${props.type}`}>
+    <div className="page-product" data-test-id={`product-page-${type}`}>
       <main className="main">
         <div className="womens-block-header header-page">
           <div className="container womens-block-header-content">
@@ -28,32 +29,37 @@ const ProductPage = (props) => {
               <div className="header-path">
                 <span>Home</span>
                 <img src={arrovNext} alt="arrow" />
-                <span>Women</span>
+                <span>{product.category}</span>
                 <img src={arrovNext} alt="arrow" />
-                <span className="active-nav">Women's tracksuit Q109</span>
+                <span className="active-nav">{product.name}</span>
               </div>
               <div className="header-share">Share</div>
             </div>
             <h2 className="womens-block-title title-page clouses-title">
-              Women's tracksuit Q109
+              {product.name}
             </h2>
             <div className="womens-block-header-interface">
               <div className="header-path">
                 <div className="asortment-description-score">
                   <img src={star} alt="score" />
-                  <img src={star}alt="score" />
+                  <img src={star} alt="score" />
                   <img src={star} alt="score" />
                   <img src={star} alt="score" />
                   <img src={star} alt="score" />
                 </div>
-                <div className="score-counter">2 Reviews</div>
+                <div className="score-counter">
+                  {product.reviews.length
+                    ? `${product.reviews.length}Reviews`
+                    : `No Reviews`}
+                </div>
               </div>
               <div className="model-description">
-                <p>SKU:<span className="model-description-acсent">777</span></p>
                 <p>
-                  Availability:<span className="model-description-acсent"
-                    >In Stock</span
-                  >
+                  SKU:<span className="model-description-acсent">777</span>
+                </p>
+                <p>
+                  Availability:
+                  <span className="model-description-acсent">In Stock</span>
                 </p>
               </div>
             </div>
@@ -61,48 +67,64 @@ const ProductPage = (props) => {
         </div>
         <div className="container">
           <div className="product-page-content">
-          <ProductSlider></ProductSlider>
+            <ProductSlider></ProductSlider>
             <div className="description-product-page">
               <div className="description-product-page__color">
                 <p className="color-model">
-                  Color: <span className="model-description-acсent">blue</span>
+                  Color:{" "}
+                  <span className="model-description-acсent">
+                    {activeColor}
+                  </span>
                 </p>
                 <div className="choise-models">
-                  <div className="choise-models-item">
-                    <img
-                      src={model1}
-                      alt="wear"
-                    />
-                  </div>
-                  <div className="choise-models-item">
-                    <img
-                      src={model2}
-                      alt="wear"
-                    />
-                  </div>
-                  <div className="choise-models-item">
-                    <img
-                      src={model3}
-                      alt="wear"
-                    />
-                  </div>
-                  <div className="choise-models-item">
-                    <img
-                      src={model4}
-                      alt="wear"
-                    />
-                  </div>
+                  {[...uniqueColors]
+                    .map((item) =>
+                      product.images.find(({ color }) => color === item)
+                    )
+                    .map(({ id, color, url }) => {
+                      return (
+                        <div
+                          className={
+                            activeIdColor === id
+                              ? "choise-models-item choise-models-item-border"
+                              : "choise-models-item"
+                          }
+                          key={id}
+                          onClick={() => {
+                            setActiveIdColor(id);
+                            setActiveColor(color);
+                          }}
+                        >
+                          <img
+                            src={`https://training.cleverland.by/shop${url}`}
+                            alt="wear"
+                          />
+                        </div>
+                      );
+                    })}
                 </div>
               </div>
               <div className="description-product-page__size">
                 <p className="size-model">
-                  Size: <span className="model-description-acсent">S</span>
+                  Size:{" "}
+                  <span className="model-description-acсent">{activeSize}</span>
                 </p>
                 <div className="size-models">
-                  <div className="size-models-item">XS</div>
-                  <div className="size-models-item size-models-item_active">S</div>
-                  <div className="size-models-item">M</div>
-                  <div className="size-models-item">L</div>
+                  {product.sizes.map((item, id) => {
+                    return (
+                      <div
+                        className={
+                          item === activeSize
+                            ? "size-models-item size-models-item_active"
+                            : "size-models-item"
+                        }
+                        key={id}
+                        onClick={() => setActiveSize(item)}
+                      >
+                        {item}
+                      </div>
+                    );
+                  })}
                 </div>
                 <p className="size-icon">Size guide</p>
               </div>
@@ -114,20 +136,14 @@ const ProductPage = (props) => {
                   <img src={heart} alt="heart" />
                 </div>
                 <div className="price-icon-scales">
-                  <img
-                    src={scales}
-                    alt="scales"
-                  />
+                  <img src={scales} alt="scales" />
                 </div>
               </div>
               <hr />
               <div className="delivery-work description-product-page__shipping">
                 <div className="work-block">
                   <div className="work-block-img">
-                    <img
-                      src={shipping}
-                      alt="work-block"
-                    />
+                    <img src={shipping} alt="work-block" />
                   </div>
                   <div className="work-block-description">
                     <p className="work-block-text">Shipping & Delivery</p>
@@ -135,10 +151,7 @@ const ProductPage = (props) => {
                 </div>
                 <div className="work-block">
                   <div className="work-block-img">
-                    <img
-                      src={return1}
-                      alt="work-block"
-                    />
+                    <img src={return1} alt="work-block" />
                   </div>
                   <div className="work-block-description">
                     <p className="work-block-text">Returns & Exchanges</p>
@@ -146,10 +159,7 @@ const ProductPage = (props) => {
                 </div>
                 <div className="work-block">
                   <div className="work-block-img">
-                    <img
-                      src={send}
-                      alt="work-block"
-                    />
+                    <img src={send} alt="work-block" />
                   </div>
                   <div className="work-block-description">
                     <p className="work-block-text">Ask a question</p>
@@ -163,19 +173,22 @@ const ProductPage = (props) => {
               </div>
               <hr />
               <div className="description-product-page__description">
-                <h3 className="guaranties-title description-title">DESCRIPTION</h3>
+                <h3 className="guaranties-title description-title">
+                  DESCRIPTION
+                </h3>
               </div>
               <hr />
               <div className="description-product-page__additional">
                 <h3 className="guaranties-title">ADDITIONAL INFORMATION</h3>
                 <p className="model-description-acсent">
-                  Color:<span className="no-accent">Blue, White, Black, Grey</span>
+                  Color:
+                  <span className="no-accent">Blue, White, Black, Grey</span>
                 </p>
                 <p className="model-description-acсent">
                   Size:<span className="no-accent">XS, S, M, L</span>
                 </p>
                 <p className="model-description-acсent">
-                  Material:<span className="no-accent"> 100% Polyester</span>
+                  Material:<span className="no-accent">{product.material}</span>
                 </p>
               </div>
               <hr />
