1cff23826347e8fd66fed462b532c29a25c4bb9e
AnzhelikaMikulich <angelika.flyorko@gmail.com>
2022-03-16 15:49:35 +0300

    fix: add style for hidden skroll

--- src/components/Header/HeaderUseful.jsx
+++ src/components/Header/HeaderUseful.jsx
@@ -1,50 +1,54 @@
-import React from 'react';
-import { useState } from 'react';
-import search from '../../assets/image/icons/lupa.png'
-import earth from '../../assets/image/icons/planet.png'
-import human from '../../assets/image/icons/person.png'
-import basket from '../../assets/image/icons/card.png'
-import ShoppingCard from '../ShoppingCard/ShoppingCard';
+import React from "react";
+import { useState } from "react";
+import search from "../../assets/image/icons/lupa.png";
+import earth from "../../assets/image/icons/planet.png";
+import human from "../../assets/image/icons/person.png";
+import basket from "../../assets/image/icons/card.png";
+import ShoppingCard from "../ShoppingCard/ShoppingCard";
 
 const HeaderUseful = () => {
   const [cardActive, setCardActive] = useState(false);
 
   function toggleCard() {
     setCardActive(!cardActive);
-    if ((!cardActive)) {
+    if (!cardActive) {
       document.body.style.overflow = "hidden";
       document.body.style.margin = "0 16px 0 0";
-   
     } else {
       document.body.style.overflow = "";
       document.body.style.margin = " 0 0px 0 0";
-      
-   
     }
   }
   return (
     <div className="header-ui">
-            <ul className="header-ui-menu">
-              <li>
-                <a className="icons-search" href="#section"><img src={search} alt="search" /></a>
-              </li>
-              <li>
-                <a className="icons-earth" href="#section"><img src={earth} alt="earth" /></a>
-              </li>
-              <li>
-                <a className="icons-human" href="#section"><img src={human} alt="human" /></a>
-              </li>
-              <li onClick={()=>toggleCard()}>
-                <div className="icons-basket">
-                  <img src={basket} alt="basket" />
-                </div>
-                <div className="bascet-counter">2</div>
-              </li>
-            </ul>
-            <ShoppingCard active={cardActive}
-        setActive={setCardActive}></ShoppingCard>
+      <ul className="header-ui-menu">
+        <li>
+          <a className="icons-search" href="#section">
+            <img src={search} alt="search" />
+          </a>
+        </li>
+        <li>
+          <a className="icons-earth" href="#section">
+            <img src={earth} alt="earth" />
+          </a>
+        </li>
+        <li>
+          <a className="icons-human" href="#section">
+            <img src={human} alt="human" />
+          </a>
+        </li>
+        <li onClick={() => toggleCard()}>
+          <div className="icons-basket">
+            <img src={basket} alt="basket" />
           </div>
-          
+          <div className="bascet-counter">2</div>
+        </li>
+      </ul>
+      <ShoppingCard
+        active={cardActive}
+        setActive={setCardActive}
+      ></ShoppingCard>
+    </div>
   );
 };
 
--- src/components/ShoppingCard/ShoppingCard.jsx
+++ src/components/ShoppingCard/ShoppingCard.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
-import './ShoppingCard.css'
-import item from '../../assets/image/productpage/slider1.png'
+import "./ShoppingCard.css";
+import item from "../../assets/image/productpage/slider1.png";
 
 const ShoppingCard = ({ active, setActive }) => {
   function toggleCard() {
@@ -9,46 +9,67 @@ const ShoppingCard = ({ active, setActive }) => {
     if (setActive(false)) {
       document.body.style.overflow = "hidden";
       document.body.style.margin = "0 16px 0 0";
-   
     } else {
       document.body.style.overflow = "";
       document.body.style.margin = "0 0px 0 0";
-   
     }
   }
-  
-  return (
-    <div className={active ? "popup-shopping-card popup-shopping-card-active" : "popup-shopping-card"} >
 
-    <div className="popap-shopping-card-body">
-      <div className="popap-shopping-card-content">
-        <div className="shoping-card-header">
-          <h2 className="shoping-card-title">Shopping Cart</h2><div onClick={() => toggleCard()} className="popap-shopping-card-close shopping-card-close-popap"></div>
-        </div>
-        <nav className="shoping-card-nav">
-          <p className="nav-shopping-active">Item in Cart</p><span>/</span><p>Delivery Info</p><span>/</span><p>Payment</p>
-        </nav>
-        <div className="shoping-card-item-container">
-          <div className="shoping-card-item">
-            <div className="shoping-card-item-img"><img src={item} alt="img"/></div>
-            <div className="shoping-card-item-description">
-              <p className="shoping-card-item-name">Women's tracksuit Q109</p>
-              <p className="shoping-card-item-color"><span>Blue</span>,<span >S</span></p>
-              <div className="shoping-card-item-count" ><div className="shoping-card-item-counter"><button className='item-counter-decrease item-counter-card'></button><span>1</span><button className='item-counter-increase item-counter-card'></button> </div> <p className="shoping-card-item-price">$54.00</p></div>
-            </div>
-            <div className="shoping-card-item-delete"></div>
+  return (
+    <div
+      className={
+        active
+          ? "popup-shopping-card popup-shopping-card-active"
+          : "popup-shopping-card"
+      }
+    >
+      <div className="popap-shopping-card-body">
+        <div className="popap-shopping-card-content">
+          <div className="shoping-card-header">
+            <h2 className="shoping-card-title">Shopping Cart</h2>
+            <div
+              onClick={() => toggleCard()}
+              className="popap-shopping-card-close shopping-card-close-popap"
+            ></div>
           </div>
-          
+          <nav className="shoping-card-nav">
+            <p className="nav-shopping-active">Item in Cart</p>
+            <span>/</span>
+            <p>Delivery Info</p>
+            <span>/</span>
+            <p>Payment</p>
+          </nav>
+          <div className="shoping-card-item-container">
+            <div className="shoping-card-item">
+              <div className="shoping-card-item-img">
+                <img src={item} alt="img" />
+              </div>
+              <div className="shoping-card-item-description">
+                <p className="shoping-card-item-name">Women's tracksuit Q109</p>
+                <p className="shoping-card-item-color">
+                  <span>Blue</span>,<span>S</span>
+                </p>
+                <div className="shoping-card-item-count">
+                  <div className="shoping-card-item-counter">
+                    <button className="item-counter-decrease item-counter-card"></button>
+                    <span>1</span>
+                    <button className="item-counter-increase item-counter-card"></button>{" "}
+                  </div>{" "}
+                  <p className="shoping-card-item-price">$54.00</p>
+                </div>
+              </div>
+              <div className="shoping-card-item-delete"></div>
+            </div>
+          </div>
+          <div className="shoping-card-total-price">
+            <p className="shoping-card-total-price-title">Total</p>
+            <p className="shoping-card-total">$433.99</p>
+          </div>
+          <button className="shoping-card-btn further-btn">Further</button>
+          <button className="shoping-card-btn viewcart-btn">View Cart</button>
         </div>
-        <div className="shoping-card-total-price">
-          <p className="shoping-card-total-price-title">Total</p>
-          <p className="shoping-card-total">$433.99</p>
-        </div>
-        <button className="shoping-card-btn further-btn">Further</button>
-        <button className="shoping-card-btn viewcart-btn">View Cart</button>
       </div>
     </div>
-  </div>
   );
 };
 
